3a7d4fcafea293ce182a25146e9dc4096c7e8bdf
Enzo Sterro <enzo.sterro@gmail.com>
2017-10-05 15:16:23 +0300

    UserDefaults extended to support storing user data

---
+++ Etherium/Helpers/Extensions/UserDefaults+Settings.swift
@@ -0,0 +1,25 @@
+//
+//  NSUserDefaults+Settings.swift
+//  Etherium
+//
+//  Created by Aleksandr Kundelev on 05/10/2017.
+//  Copyright © 2017 Aleksandr Kundelev. All rights reserved.
+//
+
+import Cocoa
+
+extension UserDefaults {
+
+    static func setCurrentCurrency(_ currency: CoinModel.name) {
+        self.standard.set(currency.rawValue, forKey: "com.enzosterro.cryprocurrency.currentCurrency")
+    }
+    
+    static func getCurrentCurrency() -> CoinModel.name {
+        if let storedValue = self.standard.value(forKey: "com.enzosterro.cryprocurrency.currentCurrency") as? String,
+            let coinModel = CoinModel.name(rawValue: storedValue) {
+            return coinModel
+        } else {
+            return CoinModel.name.bitcoin
+        }
+    }
+}
